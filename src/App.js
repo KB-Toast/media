dbf3e209db04d0ba22561ddfca916d1e3a36f060
KB-Toast <kevin.brusson@gmail.com>
2023-04-11 21:09:03 +0200

    up to redux toolkit query

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Userslist from "./components/UsersList";
+import UsersList from './components/UsersList';
 
 function App() {
   return (
     <div className="container mx-auto">
-      <Userslist />
+      <UsersList />
     </div>
   );
 }
